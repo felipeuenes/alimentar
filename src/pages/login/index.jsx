744c8a77c210b45052ce05b1981212e1b6549437
Felipe Uenes <uenesdesigner@gmail.com>
2023-09-28 19:56:23 -0300

    login

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,48 @@
+import './style.css'
+import logomarca from '../../assets/logomarca.png';
+import logoGoverno from '../../assets/cearasemfomelogo.png'
+
+
+export function Home() {
+
+
+    return(
+
+        <div className='home-fundo-geral'>
+            <div className='home-box'>
+                <section className='logos'>
+                    <section className='logoAlimentar'><img src={logomarca} alt="" /></section>
+                    <section className='logoGoverno'><img src={logoGoverno} alt="" /></section>
+                </section>
+                <section className='form'>
+                    
+                    <section className='login'><h1>LOGIN</h1></section>
+            <hr></hr>
+                    <section className='formulario'>
+                        <form action="">
+                            <section className='inputs'>
+                                <label htmlFor="email">Email:</label>
+                                <input type="text" id='email'/>
+                            </section>
+                            <section className='inputs'>
+                                <label htmlFor="senha">Senha:</label>
+                                <input type="text" id='senha'/>
+                            </section>
+                            <section>
+                                <button>Entrar</button>
+                            </section>
+                            <section>
+                                <input type="checkbox" />
+                                <a href="">
+                                    <p>Esqueceu a senha?</p>
+                                    </a>
+                            </section>
+                        </form>
+                    </section>
+
+                </section>
+            </div>
+
+        </div>
+    )
+}
