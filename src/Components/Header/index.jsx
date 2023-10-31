72246cfde965720b74440405c5bf2c795a19449e
Felipe Uenes <uenesdesigner@gmail.com>
2023-10-31 20:03:15 -0300

    add

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,33 +1,33 @@
-import './style.css';
-import logomarca from '../../assets/logomarca-menor.png'
-import iconInfo from '../../assets/icon-info.png'
-import iconConfig from '../../assets/icon-config.png'
-import iconPerfil from '../../assets/icon-perfil.png'
-import { Link } from 'react-router-dom';
+import "./style.css";
+import logomarca from "../../assets/logomarca-menor.png";
+import iconInfo from "../../assets/icon-info.png";
+import iconConfig from "../../assets/icon-config.png";
+import iconPerfil from "../../assets/icon-perfil.png";
+import { Link } from "react-router-dom";
 
-
-export function Header(){
-
-
-    return(
-        <div className='cabecalho'>
-            <section className='menu'>
-             <Link to={'/home'}> <img src={logomarca} alt="logomarca" /></Link>  
-                <ul>
-                 <li><Link to='/unidadesGestoras'>Cadastro</Link></li>
-                    <li>Lançamentos</li>
-                    <li>Relatórios</li>
-                    <div class="dropdown">
- 
-</div>
-                </ul>
-            </section>
-            <section className='configs'>
-                <img src={iconInfo} alt="" />
-                <img src={iconConfig} alt="" />
-                <img src={iconPerfil} alt="" />
-            </section>
-            
-        </div>
-    )
+export function Header() {
+  return (
+    <div className="cabecalho">
+      <section className="menu">
+        <Link to={"/home"}>
+          {" "}
+          <img src={logomarca} alt="logomarca" />
+        </Link>
+        <ul>
+          <li>
+            <Link to="/unidadesGestoras">Cadastro</Link>
+          </li>
+          <li>Lançamentos</li>
+          <li>Relatórios</li>
+          
+        </ul>
+       
+      </section>
+      <section className="configs">
+        <img src={iconInfo} alt="" />
+        <img src={iconConfig} alt="" />
+        <img src={iconPerfil} alt="" />
+      </section>
+    </div>
+  );
 }
