9f7f20fda19c33b0a1f3e966fa0aaca2ab3e7160
Felipe Uenes <uenesdesigner@gmail.com>
2023-11-02 15:23:45 -0300

    table

--- src/Components/TabelaUspr/index.jsx
+++ src/Components/TabelaUspr/index.jsx
@@ -29,8 +29,8 @@ export function TabelaUspr() {
          <thead>
             <tr>
             <th scope="col">Código</th>
-            <th scope="col">Nome da unidade gestora</th>
-            <th scope="col">CNPJ</th>
+            <th scope="col">Nome da USPR</th>
+            <th scope="col">Cidade</th>
             <th scope="col">Responsável</th>
             <th scope="col">Telefone</th>
             <th scope="col"></th>
@@ -40,7 +40,7 @@ export function TabelaUspr() {
             <tr className='linha1'>
             <th scope="row">001</th>
             <td>Unidade 1</td>
-            <td>00.000.000/0001-01</td>
+            <td>Fortaleza</td>
             <td>Reponsável 1</td>
             <td>(00) 00000-0000</td>
             <td id='icons'><img src={editar} alt="" id='editar' /><img src={lixeira} alt="" id='lixeira'/></td>
@@ -48,14 +48,14 @@ export function TabelaUspr() {
             <tr>
             <th scope="row">002</th>
             <td>Unidade 2</td>
-            <td>00.000.000/0001-01</td>
+            <td>Jucás</td>
             <td>Responsável 2</td>
             <td></td>
             </tr>
             <tr className='linha1'>
             <th scope="row">003</th>
             <td colspan="1">Unidade 3</td>
-            <td colspan="1">0000000000</td>
+            <td colspan="1">Iguatu</td>
             <td>Responsável 3</td>
             <td></td>
             <td id='icons'><img src={editar} alt="" id='editar' /><img src={lixeira} alt="" id='lixeira'/></td>
